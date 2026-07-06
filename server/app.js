9db27276ad31d8d18eb9f779b87596b5d82a999f
Nandini14056 <nandiniraulji1456@gmail.com>
2026-07-06 18:11:03 +0530

    feat: set up authentication routes and configure CORS in the server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const authRouter = require("./routes/auth.routes");
 
 connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/auth", authRouter);
 
 const port = process.env.PORT || 5000;
 
