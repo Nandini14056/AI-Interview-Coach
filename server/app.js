93d70ef783f49c928ee8bdc414b797ae64540121
Nandini14056 <nandiniraulji1456@gmail.com>
2026-07-07 18:30:29 +0530

    feat: add error handling middleware and integrate it into the server

--- server/app.js
+++ server/app.js
@@ -5,27 +5,35 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 const authRouter = require("./routes/auth.routes");
+const errorHandler = require("./middleware/error.middleware");
 
 connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRouter);
 
-const port = process.env.PORT || 5000;
-
+//Routes
 app.get("/", (req, res) => {
   res.json({
     success: true,
     message: "AI Interview Coach API is running",
   });
 });
+
+app.use("/api/auth", authRouter);
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`the server is running on port ${port}`);
